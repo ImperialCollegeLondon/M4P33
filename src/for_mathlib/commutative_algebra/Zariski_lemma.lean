0299de9963fa486c50b43e9b8c2b37ed3e58832e
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-01-26 15:38:25 +0000

    stub

---
+++ src/for_mathlib/commutative_algebra/Zariski_lemma.lean
@@ -0,0 +1,12 @@
+/-
+
+Zariski's Lemma: if k is a field and K is a field finitely generated
+as a k-algebra, then K is also finitely-generated as a k-module
+
+-/
+
+import ring_theory.integral_closure
+
+theorem Zariskis_lemma (k : Type*) [discrete_field k]
+  (K : Type*) [discrete_field K] [algebra k K] :
+0 = 0 := sorry
