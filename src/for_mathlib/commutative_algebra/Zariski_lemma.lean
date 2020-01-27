205d7d4550f8d21619f79bf276c414beb570cc24
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-01-27 11:38:03 +0000

    Statement of Zariski's Lemma

--- src/for_mathlib/commutative_algebra/Zariski_lemma.lean
+++ src/for_mathlib/commutative_algebra/Zariski_lemma.lean
@@ -7,6 +7,17 @@ as a k-algebra, then K is also finitely-generated as a k-module
 
 import ring_theory.integral_closure
 
-theorem Zariskis_lemma (k : Type*) [discrete_field k]
-  (K : Type*) [discrete_field K] [algebra k K] :
-0 = 0 := sorry
+theorem Zariskis_lemma
+  -- let k be a field
+  (k : Type*) [discrete_field k]
+  -- and let k ⊆ K be another field
+  (K : Type*) [discrete_field K] [algebra k K] 
+  -- Assume that there's a finite subset s of K
+  (s : set K) (hfs : s.finite)
+  -- which generates K as a k-algebra
+  (hsgen : algebra.adjoin k s = ⊤)
+  -- Then
+  :
+  -- K is finitely-generated as a k-vector space
+(⊤ : submodule k K).fg
+:= sorry
